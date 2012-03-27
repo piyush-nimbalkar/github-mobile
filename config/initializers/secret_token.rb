4a1c9cecee5cee9d648d3b3bd94a162745c1a67e
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2012-03-27 21:21:07 +0530

    Newly created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubMobile::Application.config.secret_token = 'a2c755acb8e563cb65c24e242d438f6d904eb3d4625f2945ffc427ea1ddd4247ab5b2d76e366e45174bbf6bb5f4fb63cd4d5125a60a444f0304e91b8fa0c38f9'
